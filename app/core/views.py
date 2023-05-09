cf6aed2977c0505a8536d9d539b186ff857fa325
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-05-09 18:41:46 -0400

    api

--- app/core/views.py
+++ app/core/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse, JsonResponse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
-
+import requests
+from django.views import View
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -15,8 +18,25 @@ def productos(request):
 def contacto(request):
     return render(request, 'paginas/contacto.html')
 
+
+
 def api(request):
-    return render(request, 'paginas/api.html')
+    # Autenticación con tu clave de API
+    SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+    SERVICE_ACCOUNT_FILE = 'ruta/a/tu/archivo.json'
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # Crea el objeto de servicio de la API de YouTube Music
+    youtube_music = build('youtubemusic', 'v1', credentials=credentials)
+
+    # Obtén las canciones más escuchadas
+    chart_response = youtube_music.chart().list(chart='mostPopular', regionCode='US').execute()
+    songs = chart_response['songs']
+
+    # Renderiza los resultados en una plantilla de Django
+    return render(request, 'paginas/api.html', {'songs': songs})
+
 
 def login(request):
     return render(request, 'paginas/login.html')
